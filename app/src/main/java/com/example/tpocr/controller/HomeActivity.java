410696d0941d49008b01d727cdc34ea036b3bcd8
Laur1502 <48560664+Laur1502@users.noreply.github.com>
2021-12-11 21:25:48 +0100

    Ajout de la page Scrore + structuration de HomeActivity

--- app/src/main/java/com/example/tpocr/controller/HomeActivity.java
+++ app/src/main/java/com/example/tpocr/controller/HomeActivity.java
@@ -2,15 +2,50 @@ package com.example.tpocr.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.tpocr.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button normalGameBtn, timedGameBtn, scoreBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        normalGameBtn = (Button) findViewById(R.id.normal_mode);
+        timedGameBtn = (Button) findViewById(R.id.timer_mode);
+        scoreBtn = (Button) findViewById(R.id.scores);
+
+        normalGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        timedGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        scoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
